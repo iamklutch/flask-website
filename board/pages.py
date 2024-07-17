eb2055f13250644fbfabd147fbfb4a4da959acb4
JamesH <yuki@yukidev.com>
2024-07-16 18:42:38 -0600

    Moved about, skills, projects, and recommendations to seperate pages and updated the blueprint and python pages modules.

--- board/pages.py
+++ board/pages.py
@@ -5,7 +5,15 @@ bp = Blueprint("pages", __name__)
 @bp.route("/")
 def home():
     return render_template("pages/home.html")
-
+@bp.route("/skills")
+def skills():
+    return render_template("pages/skills.html")
+@bp.route("/projects")
+def projects():
+    return render_template("pages/projects.html")
+@bp.route("/recommendations")
+def recommendations():
+    return render_template("pages/recommendations.html")
 @bp.route("/about")
 def about():
     return render_template("pages/about.html")
