c6bb4d1fbed76e2d19fdbddc5f3f5a01600e72db
James Higashiyama <jimmyhiggs@jeemail.com>
2024-08-31 13:01:49 -0600

    Added ITIL cert badge, link, and page with cert

--- board/pages.py
+++ board/pages.py
@@ -5,15 +5,15 @@ bp = Blueprint("pages", __name__)
 @bp.route("/")
 def home():
     return render_template("pages/home.html")
-@bp.route("/skills")
-def skills():
-    return render_template("pages/skills.html")
+@bp.route("/itilcert")
+def itilcert():
+    return render_template("pages/itilcert.html")
 @bp.route("/projects")
 def projects():
     return render_template("pages/projects.html")
-@bp.route("/recommendations")
-def recommendations():
-    return render_template("pages/recommendations.html")
-@bp.route("/about")
-def about():
-    return render_template("pages/about.html")
+# @bp.route("/recommendations")
+# def recommendations():
+#     return render_template("pages/recommendations.html")
+# @bp.route("/about")
+# def about():
+#     return render_template("pages/about.html")
