128734b03e736f5fe1ff7e478308a62c21e03d25
James Higashiyama <james.higashiyama@gmail.com>
2025-01-03 14:51:24 -0700

    Update pages.py

--- board/pages.py
+++ board/pages.py
@@ -11,9 +11,6 @@ def itilcert():
 @bp.route("/projects")
 def projects():
     return render_template("pages/projects.html")
-@bp.route("/wgudiploma")
-def wgudiploma():
-    return render_template("pages/wgudiploma.html")
 # @bp.route("/recommendations")
 # def recommendations():
 #     return render_template("pages/recommendations.html")
